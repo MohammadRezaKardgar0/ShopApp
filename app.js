c0ec7f456bed8f7935eef42a9648a37b61674917
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-25 08:54:21 +0330

    create products class

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+import Products from "./models/products.js";
 import { fetchData } from "./utils/httpReq.js";
+
+const productsNode = document.getElementById("products");
 
 const render = async () => {
   const productsData = await fetchData();
-  console.log(productsData);
+  const productsInstance = new Products(productsNode, productsData);
+  productsInstance.showProducts()
 };
 
 document.addEventListener("DOMContentLoaded", render);
