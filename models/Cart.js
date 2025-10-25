21a0db45bca8a8d8efc1ac6c62bd52fa353f86f5
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-25 11:04:28 +0330

    show buy products

--- models/Cart.js
+++ models/Cart.js
@@ -4,7 +4,22 @@ class Cart {
     this.price = price;
     this.cart = cart;
     this.products = [];
+    this.toShow = [];
   }
+
+  showProducts() {
+    this.toShow = [...new Set(this.products)];
+    this.parent.innerHTML = "";
+    this.toShow.forEach((product) => {
+      const qty = this.products.filter((p) => p.id === product.id).length;
+      this.createCard(product,qty);
+    });
+  }
+
+  createCard(data,qty){
+    console.log(data)
+  }
+
 }
 
 export default Cart;
--- models/Products.js
+++ models/Products.js
@@ -1,7 +1,8 @@
 class Products {
-  constructor(parent, products) {
+  constructor(parent, products, cart) {
     this.parent = parent;
     this.products = products;
+    this.cart = cart;
     this.parent.addEventListener("click", this);
   }
 
@@ -48,6 +49,7 @@ class Products {
   addToCart(id) {
     const product = this.products.find((i) => i.id === +id);
     this.cart.products.push(product);
+    this.cart.showProducts();
   }
 }
 
