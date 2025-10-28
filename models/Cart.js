0ae656e97e96e6897b422e1061c36861c5a0af3a
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-28 10:01:34 +0330

    create component card

--- models/Cart.js
+++ models/Cart.js
@@ -12,14 +12,52 @@ class Cart {
     this.parent.innerHTML = "";
     this.toShow.forEach((product) => {
       const qty = this.products.filter((p) => p.id === product.id).length;
-      this.createCard(product,qty);
+      this.createCard(product, qty);
     });
   }
 
-  createCard(data,qty){
-    console.log(data)
+  createCard(data, qty) {
+    const cardEle = document.createElement("div");
+    const imgEle = this.productImage(data);
+    const infoEle = this.productInfo(data);
+    const controlEle = this.productControl(data, qty);
+
+    cardEle.innerHTML = imgEle;
+    cardEle.innerHTML += infoEle;
+    cardEle.innerHTML += controlEle;
+
+    this.parent.appendChild(cardEle);
   }
 
+  productImage(data) {
+    const { image, alt } = data;
+    const imgJsx = `<img src=${image} alt=${alt}/>`;
+    return imgJsx;
+  }
+
+  productInfo(data) {
+    const { name, price } = data;
+    const infoJsx = `
+    <div>
+    <h4>${name}</h4>
+    <p>${price}</p>
+    </div>`;
+    return infoJsx;
+  }
+
+  productControl(data, qty) {
+    const { id } = data;
+    const controlJsx = `
+<div>
+    <div>
+    <button data-id=${id}>-</button>
+    <span>${qty}</span>
+    <button data-id=${id}>+</button>
+    </div>
+    <button data-id=${id}>Remove</button>
+</div>`;
+    return controlJsx;
+  }
 }
 
 export default Cart;
