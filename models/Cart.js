28670b110143cae1f34555744f8345816985b3cd
MohammadRezaKardgar <MohammadRezaKardgar.js@gmail.com>
2025-10-28 11:01:29 +0330

    handel event shop cart

--- models/Cart.js
+++ models/Cart.js
@@ -5,6 +5,7 @@ class Cart {
     this.cart = cart;
     this.products = [];
     this.toShow = [];
+    this.parent.addEventListener("click", this);
   }
 
   showProducts() {
@@ -57,6 +58,47 @@ class Cart {
     <button data-id=${id}>Remove</button>
 </div>`;
     return controlJsx;
+  }
+
+  handleEvent(event) {
+    const tagName = event.target.tagName;
+    const id = event.target.dataset.id;
+    const type = event.target.innerText;
+    if (tagName !== "BUTTON") return;
+    switch (type) {
+      case "+":
+        this.increase(id);
+        break;
+
+      case "-":
+        this.decrease(id);
+        break;
+
+      case "Remove":
+        this.remove(id);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  increase(id) {
+    const product = this.products.find((p) => p.id === +id);
+    this.products.push(product);
+    this.showProducts();
+  }
+
+  decrease(id) {
+    const index = this.products.findIndex((p) => (p.id === id));
+    this.products.splice(index, 1);
+    this.showProducts();
+  }
+
+  remove(id) {
+    const newProducts = this.products.filter((p) => p.id !== +id);
+    this.products = newProducts;
+    this.showProducts();
   }
 }
 
